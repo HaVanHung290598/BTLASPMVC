d99ac2058ee176f474c1125f0001a3b2575d9999
Hung Ha <havanhung29051998@gmail.com>
2021-07-04 18:15:31 +0700

    Init

---
+++ BTL/Areas/Admin/Controllers/AccountManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL.Areas.Admin.Controllers
+{
+    public class AccountManagerController : Controller
+    {
+        // GET: Admin/AccountManager
+        public ActionResult AccountManager()
+        {
+            return View();
+        }
+    }
+}
---
+++ BTL/Areas/Admin/Controllers/CategoryManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL.Areas.Admin.Controllers
+{
+    public class CategoryManagerController : Controller
+    {
+        // GET: Admin/CategoryManager
+        public ActionResult CategoryManager()
+        {
+            return View();
+        }
+    }
+}
